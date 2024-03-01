88f594c8a2825081a1b42e1e2f2fb6e9b65b38fe
wrenevans <161795683+wrenevans@users.noreply.github.com>
2024-03-01 16:25:27 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("BEXJBMWv7GRSgeqK6YQXC8osUs2QZ1qsxSGd3jeWSi5T");
 
 #[program]
 pub mod nft_lend_borrow {
